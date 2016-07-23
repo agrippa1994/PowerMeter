8e86abcc6e353fa9b29bb0629e677cc3ba7bb631
agrippa1994 <agrippa1994@icloud.com>
2016-07-24 01:01:28 +0200

    Improved performance

--- obdsocket.cpp
+++ obdsocket.cpp
@@ -1,7 +1,6 @@
 #include "obdsocket.h"
 #include <QList>
 #include <vector>
-#include <QTimer>
 
 OBDSocket::OBDSocket(QObject *parent)
     : QTcpSocket(parent)
@@ -17,76 +16,78 @@ OBDSocket::OBDSocket(QObject *parent)
     });
 
     QObject::connect(this, &QTcpSocket::readyRead, [&]() {
+        // Read all data and split lines
         QList<QString> data = QString(readAll()).split("\r\n", QString::SkipEmptyParts);
-        qDebug() << "Read: " << data;
+
         if(data.size() == 0)
             return;
 
-        if(data.contains(">")) {
+        qDebug() << "Read: " << data;
+
+        if(data.last() == ">") {
             obdReady = true;
         }
 
-        QTimer::singleShot(5, [=]() {
-            // Read lines
-            for(const QString& line : data) {
-                std::vector<int> bytes;
+        // Read lines
+        for(const QString& line : data) {
+            std::vector<int> bytes;
 
-                // Parse bytes
-                for(QString token : line.split(' ', QString::SkipEmptyParts)) {
-                    bool ok = false;
-                    int parsed = token.toInt(&ok, 16);
-                    if(ok) {
-                        bytes.push_back(parsed);
-                    }
-                    else {
-                        if(line != ">" && line.length() > 0)
-                            emit stringRead(line);
-                        break;
-                    }
+            // Parse bytes
+            for(QString token : line.split(' ', QString::SkipEmptyParts)) {
+                bool ok = false;
+                int parsed = token.toInt(&ok, 16);
+                if(ok) {
+                    bytes.push_back(parsed);
                 }
+                else {
+                    if(line != ">" && line.length() > 0)
+                        emit stringRead(line);
+                    break;
+                }
+            }
 
-                // 2 Byte (41 PID)
-                if(bytes.size() > 2) {
-                    qDebug() << "Read Bytes: " << bytes;
+            // 2 Byte (41 PID)
+            if(bytes.size() > 2) {
+                qDebug() << "Read Bytes: " << bytes;
 
-                    if(bytes[0] == 0x41) { // Check response code
-                        // Calculate MAF and HP
-                        if(bytes.size() == 4) {
-                            if(bytes[1] == 0x10) { // MAF
-                                double maf = (double)(256.0 * bytes[2] + bytes[3]) / 100.0;
-                                emit powerRead(maf / 15.2 * 46.0 * 0.33 * 1.34);
-                            }
+                if(bytes[0] == 0x41) { // Check response code
+                    // Calculate MAF and HP
+                    if(bytes.size() == 4) {
+                        if(bytes[1] == 0x10) { // MAF
+                            double maf = (double)(256.0 * bytes[2] + bytes[3]) / 100.0;
+                            emit powerRead(maf / 15.2 * 46.0 * 0.33 * 1.34);
                         }
-                        if(bytes.size() == 3) {
-                            if(bytes[1] == 0x11) { // Throttle
-                                emit throttleRead((double)bytes[2] * (100.0/255.0));
-                            }
-                            if(bytes[1] == 0x0D) { // Speed
-                                emit speedRead((double)bytes[2]);
-                            }
+                    }
+                    if(bytes.size() == 3) {
+                        if(bytes[1] == 0x11) { // Throttle
+                            emit throttleRead((double)bytes[2] * (100.0/255.0));
+                        }
+                        if(bytes[1] == 0x0D) { // Speed
+                            emit speedRead((double)bytes[2]);
                         }
                     }
                 }
             }
+        }
 
-            // Request next value
-            int req = nextRequest;
-            if(nextRequest == 0x10) req = 0x11;
-            if(nextRequest == 0x11) req = 0x0D;
-            if(nextRequest == 0x0D) req = 0x10;
+        // Request next value
+        int req = nextRequest;
+        if(nextRequest == 0x10) req = 0x11;
+        if(nextRequest == 0x11) req = 0x0D;
+        if(nextRequest == 0x0D) req = 0x10;
+
+        if(send(QString().sprintf("01 %02X 1", nextRequest)))
             nextRequest = req;
 
-            send(QString().sprintf("01 %02X 1", nextRequest));
-        });
     });
 
 }
 
 bool OBDSocket::send(const QString &data)
 {
-    qDebug() << "send: " << data;
     if(obdReady) {
         obdReady = false;
+        qDebug() << "send: " << data;
         return write((data + "\r").toStdString().c_str()) > 0;
     }
 
